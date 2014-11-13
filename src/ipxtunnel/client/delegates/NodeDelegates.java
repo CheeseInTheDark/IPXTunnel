81e25c463a5ee49f1d876ef1ee475fad8e8238aa
CheeseInTheDark <blockbattle@gmail.com>
2014-11-12 23:07:36 -0500

    Whitespace

--- src/ipxtunnel/client/delegates/NodeDelegates.java
+++ src/ipxtunnel/client/delegates/NodeDelegates.java
@@ -3,11 +3,8 @@ package ipxtunnel.client.delegates;
 
 public class NodeDelegates
 {
-
     public NodeDelegate get(int i)
     {
-        // TODO Auto-generated method stub
         return null;
     }
-
 }
